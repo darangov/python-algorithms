39183843c9de6a860fbbdd724f839751b39568e5
darangov <darangov@gmail.com>
2018-08-01 22:41:42 -0500

    update Classifier file

--- Classifier.py
+++ Classifier.py
@@ -41,6 +41,53 @@ url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pandas.read_csv(url, names=names)
 
+"""
+Formas de cargar datasets
+# Load CSV (using python)
+import csv
+import numpy
+filename = 'pima-indians-diabetes.data.csv'
+raw_data = open(filename, 'rt')
+reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE)
+x = list(reader)
+data = numpy.array(x).astype('float')
+print(data.shape)
+
+
+
+# Load CSV with Numpy
+import numpy
+filename = 'pima-indians-diabetes.data.csv'
+raw_data = open(filename, 'rt')
+data = numpy.loadtxt(raw_data, delimiter=",")
+print(data.shape)
+
+
+# Load CSV from URL using NumPy
+from numpy import loadtxt
+from urllib.request import urlopen
+url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv'
+raw_data = urlopen(url)
+dataset = loadtxt(raw_data, delimiter=",")
+print(dataset.shape)
+
+# Load CSV using Pandas
+import pandas
+filename = 'pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = pandas.read_csv(filename, names=names)
+print(data.shape)
+
+
+# Load CSV using Pandas from URL
+import pandas
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = pandas.read_csv(url, names=names)
+print(data.shape)
+
+"""
+
 # Dimensiones del DataSet
 # shape-Tamano
 print(dataset.shape)
